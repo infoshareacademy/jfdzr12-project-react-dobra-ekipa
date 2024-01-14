824a18bd5fe8a098285eddb70cb906201d37b66c
Mateusz Jamrocha <matper92@gmail.com>
2024-01-14 16:34:44 +0100

    task 1 completed

---
+++ my-app/src/Slogan.jsx
@@ -0,0 +1,7 @@
+const Slogan =() =>{
+    return <div className='Slogan'>
+        <div>Lorem Ipsum</div>
+        <img className='sloganImage' src='images/p1.jpg'></img>
+    </div>
+}
+export default Slogan
