5773bb2d0fd4dfb7ea923c308754110f7c0ce589
Mateusz Jamrocha <matper92@gmail.com>
2024-01-21 16:54:30 +0100

    modified Slogan.jsx

--- my-app/src/Slogan.jsx
+++ my-app/src/Slogan.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
+
 const Slogan =() =>{
+    let text
+    const [state, setState] = useState('text1')
+    setTimeout(()=>{setState('text2')}, 5000)
+    if (state == 'text2'){
+    text = 'Lorem ipsum'
+    }
+    else {text = 'Dolor sit ames'}
     return <div className='Slogan'>
-        <div>Lorem Ipsum</div>
+        <div>{text}</div>
         <img className='sloganImage' src='images/p1.jpg'></img>
     </div>
+    
 }
 export default Slogan
