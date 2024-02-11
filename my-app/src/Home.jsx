a1c67a15f9a8049fdba6229e6e4b557b614983ef
Michał Ceterski <132226281+michal-ceterski@users.noreply.github.com>
2024-02-11 16:15:27 +0100

    21st January 2024
    Pliki z zajęć w niedzielę 21 stycznia 2024.

---
+++ my-app/src/Home.jsx
@@ -0,0 +1,16 @@
+import Slogan from './Slogan.jsx';
+import Price from './Price.jsx';
+import PriceList from './Price-list.jsx';
+import Gallery from './Gallery.jsx';
+
+export const Home = () => {
+    return (
+      <>
+        <div>Home</div>
+        <Slogan></Slogan>
+        <Price></Price> 
+        <PriceList></PriceList>
+        {/* <Gallery></Gallery> */}
+      </>
+    );
+  };
